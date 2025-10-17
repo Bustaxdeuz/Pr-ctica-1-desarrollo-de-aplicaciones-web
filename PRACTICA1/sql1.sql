c7e67942223474c1021a77cfe9c997cac39001e9
Bustaxdeuz <fabianbustamantemdi@gmail.com>
2025-10-16 23:09:05 -0600

    Add files via upload

---
+++ PRACTICA1/sql1.sql
@@ -0,0 +1,5 @@
+INSERT INTO arbol (nombre_comun, tipo_flor, dureza_madera, altura_maxima, ruta_imagen) 
+VALUES 
+('Roble', 'Sin flor vistosa', 'Muy dura', 25.50, '/images/roble.jpg'),
+('Ceiba', 'Blanca', 'Blanda', 40.00, '/images/ceiba.jpg'),
+('Guanacaste', 'Naranja', 'Media', 30.00, '/images/guanacaste.jpg');
